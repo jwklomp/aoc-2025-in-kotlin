5d9340d77ee1daf6ef812fe01dde6f220165891f
jwklomp <jan-willem.klomp@visma.com>
2025-12-01 17:17:01 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        input.println()
-        return input.size
+    fun parseMutations(instructions: List<String>) = instructions.map {
+        val steps = it.drop(1).toInt()
+        if (it[0] == 'L') -steps else steps
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        parseMutations(input)
+            .scan(50) { position, step ->
+                Math.floorMod(position + step, 100)
+            }.count { it == 0 }
 
-    val testInput = readInput("Day01_test")
-    part1(testInput).println()
-    //part2(testInput).println()
+    fun part2(input: List<String>): Int =
+        parseMutations(input).fold(50 to 0) { (position, count), step ->
+            val newPosition = Math.floorMod(position + step, 100)
+            val crossings = when {
+                step > 0 -> Math.floorDiv(position + step, 100) - Math.floorDiv(position, 100)
+                step < 0 -> Math.floorDiv(position - 1, 100) - Math.floorDiv(position + step - 1, 100)
+                else -> 0
+            }
+            newPosition to count + crossings
+        }.second
 
     val input = readInput("Day01")
-    //part1(input).println()
-    //part2(input).println()
+    part1(input).println()
+    part2(input).println()
 }
