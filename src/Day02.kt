e9e0ea933a9731c11194bddc435a06dc3be9949b
jwklomp <jan-willem.klomp@visma.com>
2025-12-02 07:05:53 +0100

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        input.println()
-        return input.size
+    fun makeRangesOfAllNumbersAsString(input: List<String>): List<String> =
+        input.first()
+            .split(',')
+            .map { s -> s.split('-').map { it.toLong() } }
+            .map { range -> range.first()..range.last() }
+            .flatMap { intRange -> intRange.map { num -> num.toString() } }
+
+    fun isEntirelyRepeatingSubstring(s: String): Boolean =
+        (1..(s.length / 2))
+            .any { size -> s.length % size == 0 && s.chunked(size).all { it == s.take(size) } }
+
+    fun part1(input: List<String>): Long {
+        val invalids = makeRangesOfAllNumbersAsString(input)
+            .filter { it.length % 2 == 0 }
+            .filter { it.substring(0, it.length / 2) == it.substring(it.length / 2) }
+        return invalids.sumOf { it.toLong() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val invalids = makeRangesOfAllNumbersAsString(input)
+            .filter { isEntirelyRepeatingSubstring(it) }
+        return invalids.sumOf { it.toLong() }
     }
 
-    val testInput = readInput("Day01_test")
-    part1(testInput).println()
-    //part2(testInput).println()
-
-    val input = readInput("Day01")
-    //part1(input).println()
-    //part2(input).println()
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
 }
