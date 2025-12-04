4f1ade5c6066a809e8cef42b34d4c9161e8bd27f
jwklomp <jan-willem.klomp@visma.com>
2025-12-04 06:33:42 +0100

    Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,18 +1,37 @@
 fun main() {
+    val roll = "@"
+
+    fun makeFloorGrid(input: List<String>): Grid2D<String> {
+        val chunked = input.filterNot { it.isEmpty() }.map { it.chunked(1).toMutableList() }
+        return Grid2D(chunked)
+    }
+
+    fun getAccessibleRolls(floorGrid: Grid2D<String>, rolls: List<Cell<String>>): List<Cell<String>> = rolls
+        .filter { cell -> floorGrid.getSurrounding(cell.x, cell.y).count { it.value == roll } < 4 }
+
     fun part1(input: List<String>): Int {
-        input.println()
-        return input.size
+        val floorGrid = makeFloorGrid(input)
+        val rolls = floorGrid.getAllCells().filter { it.value == roll }
+        return getAccessibleRolls(floorGrid, rolls).size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val floorGrid = makeFloorGrid(input)
+        val startNumberOfRolls = floorGrid.getAllCells().count { it.value == roll }
+
+        tailrec fun removeAccessible() {
+            val rolls = floorGrid.getAllCells().filter { it.value == roll }
+            val accessible = getAccessibleRolls(floorGrid, rolls)
+            if (accessible.isEmpty()) return
+            accessible.forEach { cell -> floorGrid.setCell(cell.x, cell.y, ".") }
+            removeAccessible()
+        }
+        removeAccessible()
+        val endNumberOfRolls = floorGrid.getAllCells().count { it.value == roll }
+        return startNumberOfRolls - endNumberOfRolls
     }
 
-    val testInput = readInput("Day01_test")
-    part1(testInput).println()
-    //part2(testInput).println()
-
-    val input = readInput("Day01")
-    //part1(input).println()
-    //part2(input).println()
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
 }
